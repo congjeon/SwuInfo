1727c1d2d1a61299736c9b578ce9bb61489e9092
congjeon <wjs982247@gmail.com>
2017-07-27 10:19:22 +0900

    start

--- app/src/main/java/app/swuinfo/guru/com/swuinfo/MainActivity.java
+++ app/src/main/java/app/swuinfo/guru/com/swuinfo/MainActivity.java
@@ -1,13 +1,16 @@
 package app.swuinfo.guru.com.swuinfo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startActivity(new Intent(MainActivity.this, SplashActivity.class));
     }
 }
--- app/src/main/java/app/swuinfo/guru/com/swuinfo/SplashActivity.java
+++ app/src/main/java/app/swuinfo/guru/com/swuinfo/SplashActivity.java
@@ -1,13 +1,24 @@
 package app.swuinfo.guru.com.swuinfo;
 
+import android.app.Activity;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                finish();
+                overridePendingTransition(R.anim.anim_slide_in_right, R.anim.anim_slide_out_left);
+            }
+        }, 3000);
     }
 }
