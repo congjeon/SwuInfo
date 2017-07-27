a92c1ca5272b9a834d70eb467075cf1ee6605359
congjeon <wjs982247@gmail.com>
2017-07-27 16:53:06 +0900

    1

--- app/src/main/java/app/swuinfo/guru/com/swuinfo/MainActivity.java
+++ app/src/main/java/app/swuinfo/guru/com/swuinfo/MainActivity.java
@@ -17,11 +17,35 @@ public class MainActivity extends CommonActivity  {
 
         startActivity(new Intent(MainActivity.this, SplashActivity.class));
 
-        findViewById(R.id.btnMenu).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, MenuActivity.class));
-            }
-        });
+        findViewById(R.id.btnNotice).setOnClickListener(clickButton);
+        findViewById(R.id.btnSchedule).setOnClickListener(clickButton);
+        findViewById(R.id.btnSite).setOnClickListener(clickButton);
+        findViewById(R.id.btnCall).setOnClickListener(clickButton);
+        findViewById(R.id.btnShuttle).setOnClickListener(clickButton);
     }
+
+    private View.OnClickListener clickButton = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent menuIntent = new Intent(MainActivity.this, MenuActivity.class);
+            switch (view.getId()) {
+                case R.id.btnNotice:
+                    menuIntent.putExtra("where", 1);
+                    break;
+                case R.id.btnSchedule:
+                    menuIntent.putExtra("where", 2);
+                    break;
+                case R.id.btnSite:
+                    menuIntent.putExtra("where", 3);
+                    break;
+                case R.id.btnCall:
+                    menuIntent.putExtra("where", 4);
+                    break;
+                case R.id.btnShuttle:
+                    menuIntent.putExtra("where", 5);
+                    break;
+            }
+            startActivity(menuIntent);
+        }
+    };
 }
