631195a7655ed1e2ad509cbc209faaf17ee5e907
Chaenaun <tictoe17@gmail.com>
2017-07-31 15:50:15 +0900

    update

--- app/src/main/java/app/swuinfo/guru/com/swuinfo/CallFragment.java
+++ app/src/main/java/app/swuinfo/guru/com/swuinfo/CallFragment.java
@@ -7,6 +7,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
+import android.telecom.Call;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +15,9 @@ import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -25,6 +29,7 @@ public class CallFragment extends Fragment {
     private Button btn1, btn2, btn3;
     private ListView listView;
     private CallListAdapter adapter;
+    private CallBean callBean;
 
 
     public CallFragment() {
@@ -32,28 +37,78 @@ public class CallFragment extends Fragment {
     }
 
 
+    private ListView m_ListView;
+    private CallListAdapter m_Adapter;
+    private List<CallBean> items;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
 
-        //현재 view에서 find view by id하기 위한 뷰 지정
+
         View view = inflater.inflate(R.layout.fragment_call, container, false);
 
-        editText = (EditText) view.findViewById(R.id.fragEdtFilter);
-        btn1 = (Button) view.findViewById(R.id.fragBtn1);
-        btn2 = (Button) view.findViewById(R.id.fragBtn2);
-        btn3 = (Button) view.findViewById(R.id.fragBtn3);
 
-        listView = (ListView) view.findViewById(R.id.fragListView);
+        view.findViewById(R.id.fragBtn1).setOnClickListener(CallClick);
+        view.findViewById(R.id.fragBtn2).setOnClickListener(CallClick);
+        view.findViewById(R.id.fragBtn3).setOnClickListener(CallClick);
 
-        //adapter = new CallListAdapter(this);
-        //listView.setAdapter(adapter);
+        CallBean call1 = new CallBean("정보전산팀", "010-9999-9999");
 
-        //adapter.addItem(ContextCompat.getDrawable(CallFragment.this, R.drawable.telephone),"학생지원팀");
-        //adapter.addItem(ContextCompat.getDrawable(CallActivity.this, R.drawable.telephone),"정보전산팀");
+        //ArrayList<String> m_List = new ArrayList<>();
+
+        ArrayList<CallBean> list = new ArrayList<CallBean>();
+        list.add(call1);
+
+        /*m_List.add("건축팀");
+        m_List.add("시설관재팀");
+        m_List.add("재무팀");
+        m_List.add("경영기획팀");
+        m_List.add("전략평가팀");
+        m_List.add("전략평가팀");
+        m_List.add("전략평가팀");
+        m_List.add("전략평가팀");
+        m_List.add("전략평가팀");
+        m_List.add("전략평가팀");
+        m_List.add("전략평가팀");
+        m_List.add("전략평가팀");
+        m_List.add("전략평가팀");
+        m_List.add("전략평가팀");
+        */
+        /*items.add(new CallBean.Item("정보전산팀", "010-9999-9999"));
+        items.add(new CallBean.Item("건축팀", "010-9999-9999"));
+        */// ListView에 아이템 추가
+        /*m_List.add("정보전산팀");
+        m_List.add("건축팀");
+        m_List.add("시설관재팀");
+        m_List.add("재무팀");
+        m_List.add("경영기획팀");
+        m_List.add("전략평가팀");
+        m_List.add("전략평가팀");
+        m_List.add("전략평가팀");
+        m_List.add("전략평가팀");
+        m_List.add("전략평가팀");
+        m_List.add("전략평가팀");
+        m_List.add("전략평가팀");
+        m_List.add("전략평가팀");
+        m_List.add("전략평가팀");
+        m_List.add("전략평가팀");*/
+
+        //어댑터 생성
+        m_Adapter = new CallListAdapter(list);
+
+        // Xml에서 추가한 ListView 연결
+        m_ListView = (ListView) view.findViewById(R.id.fragListView);
+
+        // ListView에 어댑터 연결
+        m_ListView.setAdapter(m_Adapter);
 
 
+
+
+
+        /*
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView adapterView, View view, int position, long id) {
@@ -66,52 +121,52 @@ public class CallFragment extends Fragment {
         });
 
 
-        btn1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(Intent.ACTION_CALL, Uri.parse("tel:010-0000-0000"));
-                try {
-                    startActivity(i);
-                } catch (Exception e ) {
-                    e.printStackTrace();
-                }
-            }
-        });
-
-        btn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(Intent.ACTION_CALL, Uri.parse("tel:010-1111-1111"));
-                try {
-                    startActivity(i);
-                } catch (Exception e ) {
-                    e.printStackTrace();
-                }
-            }
-        });
-
-        btn3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(Intent.ACTION_CALL, Uri.parse("tel:010-2222-2222"));
-                try {
-                    startActivity(i);
-                } catch (Exception e ) {
-                    e.printStackTrace();
-                }
-            }
-        });
-
-
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView adapterView, View view, int i, long l) {
                 String strText = (String) adapterView.getItemAtPosition(i);
             }
         });
+        */
 
 
         return view;
-    }
+
+
+    } // end OnCreate
+
+    private View.OnClickListener CallClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()) {
+                case R.id.fragBtn1:
+                    Intent i1 = new Intent(Intent.ACTION_CALL, Uri.parse("tel:010-0000-0000"));
+                    try {
+                        startActivity(i1);
+                    } catch (Exception e ) {
+                        e.printStackTrace();
+                    }
+                    break;
+                case R.id.fragBtn2:
+                    Intent i2 = new Intent(Intent.ACTION_CALL, Uri.parse("tel:010-1111-1111"));
+                    try {
+                        startActivity(i2);
+                    } catch (Exception e ) {
+                        e.printStackTrace();
+                    }
+                    break;
+                case R.id.fragBtn3:
+                    Intent i3 = new Intent(Intent.ACTION_CALL, Uri.parse("tel:010-2222-2222"));
+                    try {
+                        startActivity(i3);
+                    } catch (Exception e ) {
+                        e.printStackTrace();
+                    }
+
+            } // end switch
+
+        }
+
+    }; //end CallClick
 
 }
--- app/src/main/java/app/swuinfo/guru/com/swuinfo/CallListAdapter.java
+++ app/src/main/java/app/swuinfo/guru/com/swuinfo/CallListAdapter.java
@@ -1,13 +1,18 @@
 package app.swuinfo.guru.com.swuinfo;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,57 +25,93 @@ import static app.swuinfo.guru.com.swuinfo.R.layout.lay_call_list;
 
 public class CallListAdapter extends BaseAdapter {
 
-    private Context mContext;
-    private List<CallListItem> callListItemList = new ArrayList<CallListItem>();
+    // 문자열을 보관 할 ArrayList
+    private List<CallBean> m_List;
+    private CallBean callBean;
 
-    public CallListAdapter(Context context) {
-        mContext=context;
+    // 생성자
+    public CallListAdapter(ArrayList<CallBean> list) {
+        this.m_List = list;
     }
 
-
+    // 현재 아이템의 수를 리턴
     @Override
     public int getCount() {
-        return callListItemList.size();
+        return m_List.size();
     }
 
+    // 현재 아이템의 오브젝트를 리턴, Object를 상황에 맞게 변경하거나 리턴받은 오브젝트를 캐스팅해서 사용
     @Override
-    public Object getItem(int positon) {
-        return callListItemList.get(positon);
+    public Object getItem(int position) {
+        return m_List.get(position);
     }
 
+    // 아이템 position의 ID 값 리턴
     @Override
-    public long getItemId(int positon) {
-        return positon;
+    public long getItemId(int position) {
+        return position;
     }
 
+    // 출력 될 아이템 관리
     @Override
-    public View getView(int positon, View convertview, ViewGroup viewGroup) {
+    public View getView(int position, View convertView, ViewGroup parent) {
+        final int pos = position;
+        final Context context = parent.getContext();
 
-        final Context context = viewGroup.getContext();
+        // view가 null일 경우 커스텀 레이아웃을 얻어 옴
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        convertView = inflater.inflate(R.layout.lay_call_list, parent, false);
 
-        if(convertview == null) {
-            LayoutInflater li = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            convertview = li.inflate(R.layout.lay_call_list, null, false);
-        }
+        final CallBean  bean = m_List.get(position);
 
-        ImageView imgCallList = (ImageView)convertview.findViewById(R.id.imgCallList);
-        TextView txtCallList = (TextView)convertview.findViewById(R.id.txtCallList);
+        TextView text = (TextView) convertView.findViewById(R.id.txtCallList);
+        text.setText( bean.getTeam() );
 
-        CallListItem callListItem = callListItemList.get(positon);
+        // 버튼을 터치 했을 때 이벤트 발생
+        ImageButton btn = (ImageButton) convertView.findViewById(R.id.btnCallList);
 
-        imgCallList.setImageDrawable(callListItem.getImgCallList());
-        txtCallList.setText(callListItem.getTxtCallList());
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-        return convertview;
+                Toast.makeText(context, "전화 거는 중 : "+ bean.getTeam(), Toast.LENGTH_SHORT).show();
+                Intent i1 = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + bean.getTeam_Tel()));
+                try {
+                    context.startActivity(i1);
+                } catch (Exception e ) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+   /*     // 리스트 아이템을 터치 했을 때 이벤트 발생
+        convertView.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // 터치 시 해당 아이템 이름 출력
+                Toast.makeText(context, "전화 거는 중 : " + m_List.get(pos), Toast.LENGTH_SHORT).show();
+                Intent i1 = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + m_List.get(pos)));
+                try {
+                    context.startActivity(i1);
+                } catch (Exception e ) {
+                    e.printStackTrace();
+                }
+            }
+        });*/
+
+
+        return convertView;
     }
+/*
+    // 외부에서 아이템 추가 요청 시 사용
+    public void add(String _msg) {
+        m_List.add(_msg);
+    }*/
 
-    public void addItem(Drawable icon, String txt) {
-        CallListItem item = new CallListItem();
-
-        item.setImgCallList(icon);
-        item.setTxtCallList(txt);
-
-        callListItemList.add(item);
+    // 외부에서 아이템 삭제 요청 시 사용
+    public void remove(int _position) {
+        m_List.remove(_position);
     }
-
 }
