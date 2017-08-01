9668a2961889d0a3f30363f74bf20e81fff23f29
Chaenaun <tictoe17@gmail.com>
2017-08-01 10:58:22 +0900

    update

--- app/src/main/java/app/swuinfo/guru/com/swuinfo/CallFragment.java
+++ app/src/main/java/app/swuinfo/guru/com/swuinfo/CallFragment.java
@@ -54,46 +54,84 @@ public class CallFragment extends Fragment {
         view.findViewById(R.id.fragBtn2).setOnClickListener(CallClick);
         view.findViewById(R.id.fragBtn3).setOnClickListener(CallClick);
 
-        CallBean call1 = new CallBean("정보전산팀", "010-9999-9999");
+        CallBean call1 = new CallBean("교과/강의실지원", "02-970-5020");
+        CallBean call2 = new CallBean("수업/수강신청(전공)", "02-970-5022");
+        CallBean call3 = new CallBean("수업/수강신청(교양)", "02-970-5032");
+        CallBean call4 = new CallBean("계절학기/강의실지원", "02-970-5023");
+        CallBean call5 = new CallBean("학적/졸업/강의평가", "02-970-5021");
+        CallBean call6 = new CallBean("휴복학/복수부전공/전과/성적", "02-970-5025");
+        CallBean call7 = new CallBean("강사료/전자출결/e-class", "02-970-5024");
+        CallBean call8 = new CallBean("학력조회/결강사유서", "02-970-5026");
+        CallBean call9 = new CallBean("학점교류", "02-970-5023");
+        CallBean call10 = new CallBean("ACE사업", "02-970-5028");
+        CallBean call11 = new CallBean("SWU119(학사상담)", "02-970-1119");
+        CallBean call12 = new CallBean("장학/국가근로", "02-970-5062");
+        CallBean call13 = new CallBean("장학", "02-970-5063");
+        CallBean call14 = new CallBean("증명서발급", "02-970-5064");
+        CallBean call15 = new CallBean("국가근로", "02-970-5066");
+        CallBean call16 = new CallBean("학생지원", "02-970-5068");
+        CallBean call17 = new CallBean("보건실", "02-970-5075");
+        CallBean call18 = new CallBean("진로상담", "02-970-7888");
+
+        CallBean call19 = new CallBean("경비실-종합상황실", "02-970-1000");
+        CallBean call20 = new CallBean("경비실-행정관(주관)", "02-970-5181");
+        CallBean call21 = new CallBean("경비실-정문", "02-970-5183");
+        CallBean call22 = new CallBean("경비실-남문", "02-970-5185");
+        CallBean call23 = new CallBean("경비실-바롬관1층", "02-970-5193");
+        CallBean call24 = new CallBean("경비실-샬롬하우스", "02-970-5194");
+        CallBean call25 = new CallBean("경비실-국제교육관", "02-970-5198");
+        CallBean call26 = new CallBean("경비실-50주년기념관(주관)", "02-970-7560");
+        CallBean call27 = new CallBean("경비실-아름관,다움관", "02-970-7950");
+        CallBean call28 = new CallBean("학력조회/결강사유서", "02-970-5026");
+        CallBean call29 = new CallBean("학력조회/결강사유서", "02-970-5026");
+        CallBean call30 = new CallBean("학력조회/결강사유서", "02-970-5026");
+        CallBean call31 = new CallBean("학력조회/결강사유서", "02-970-5026");
+        CallBean call32 = new CallBean("학력조회/결강사유서", "02-970-5026");
+        CallBean call33 = new CallBean("학력조회/결강사유서", "02-970-5026");
+        CallBean call34 = new CallBean("학력조회/결강사유서", "02-970-5026");
+        CallBean call35 = new CallBean("학력조회/결강사유서", "02-970-5026");
+        CallBean call36 = new CallBean("학력조회/결강사유서", "02-970-5026");
+        CallBean call37 = new CallBean("학력조회/결강사유서", "02-970-5026");
+        CallBean call38 = new CallBean("학력조회/결강사유서", "02-970-5026");
+        CallBean call39 = new CallBean("학력조회/결강사유서", "02-970-5026");
+        CallBean call40 = new CallBean("학력조회/결강사유서", "02-970-5026");
+
+
+
+
 
         //ArrayList<String> m_List = new ArrayList<>();
 
         ArrayList<CallBean> list = new ArrayList<CallBean>();
         list.add(call1);
+        list.add(call2);
+        list.add(call3);
+        list.add(call4);
+        list.add(call5);
+        list.add(call6);
+        list.add(call7);
+        list.add(call8);
+        list.add(call9);
+        list.add(call10);
+        list.add(call11);
+        list.add(call12);
+        list.add(call13);
+        list.add(call14);
+        list.add(call15);
+        list.add(call16);
+        list.add(call17);
+        list.add(call18);
+        list.add(call19);
+        list.add(call20);
+        list.add(call21);
+        list.add(call22);
+        list.add(call23);
+        list.add(call24);
+        list.add(call25);
 
-        /*m_List.add("건축팀");
-        m_List.add("시설관재팀");
-        m_List.add("재무팀");
-        m_List.add("경영기획팀");
-        m_List.add("전략평가팀");
-        m_List.add("전략평가팀");
-        m_List.add("전략평가팀");
-        m_List.add("전략평가팀");
-        m_List.add("전략평가팀");
-        m_List.add("전략평가팀");
-        m_List.add("전략평가팀");
-        m_List.add("전략평가팀");
-        m_List.add("전략평가팀");
-        m_List.add("전략평가팀");
-        */
-        /*items.add(new CallBean.Item("정보전산팀", "010-9999-9999"));
-        items.add(new CallBean.Item("건축팀", "010-9999-9999"));
-        */// ListView에 아이템 추가
-        /*m_List.add("정보전산팀");
-        m_List.add("건축팀");
-        m_List.add("시설관재팀");
-        m_List.add("재무팀");
-        m_List.add("경영기획팀");
-        m_List.add("전략평가팀");
-        m_List.add("전략평가팀");
-        m_List.add("전략평가팀");
-        m_List.add("전략평가팀");
-        m_List.add("전략평가팀");
-        m_List.add("전략평가팀");
-        m_List.add("전략평가팀");
-        m_List.add("전략평가팀");
-        m_List.add("전략평가팀");
-        m_List.add("전략평가팀");*/
+
+
+
 
         //어댑터 생성
         m_Adapter = new CallListAdapter(list);
--- app/src/main/java/app/swuinfo/guru/com/swuinfo/CallListAdapter.java
+++ app/src/main/java/app/swuinfo/guru/com/swuinfo/CallListAdapter.java
@@ -1,18 +1,22 @@
 package app.swuinfo.guru.com.swuinfo;
 
+import android.Manifest;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
-import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.support.v4.app.Fragment;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -62,10 +66,10 @@ public class CallListAdapter extends BaseAdapter {
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         convertView = inflater.inflate(R.layout.lay_call_list, parent, false);
 
-        final CallBean  bean = m_List.get(position);
+        final CallBean bean = m_List.get(position);
 
         TextView text = (TextView) convertView.findViewById(R.id.txtCallList);
-        text.setText( bean.getTeam() );
+        text.setText(bean.getTeam());
 
         // 버튼을 터치 했을 때 이벤트 발생
         ImageButton btn = (ImageButton) convertView.findViewById(R.id.btnCallList);
@@ -74,13 +78,202 @@ public class CallListAdapter extends BaseAdapter {
             @Override
             public void onClick(View v) {
 
-                Toast.makeText(context, "전화 거는 중 : "+ bean.getTeam(), Toast.LENGTH_SHORT).show();
-                Intent i1 = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + bean.getTeam_Tel()));
-                try {
-                    context.startActivity(i1);
-                } catch (Exception e ) {
-                    e.printStackTrace();
-                }
+                /*if (bean.getTeam().toString().equals("취업상담")) {
+                    AlertDialog.Builder alertBuilder = new AlertDialog.Builder(context);
+                    alertBuilder.setTitle("번호를 선택하세요.");
+
+                    final ArrayAdapter<String> adapter = new ArrayAdapter<String>(context, android.R.layout.select_dialog_singlechoice);
+                    adapter.add("02-970-5081");
+                    adapter.add("02-970-5082");
+                    adapter.add("02-970-5083");
+                    adapter.add("02-970-5084");
+                    adapter.add("02-970-5085");
+
+                    alertBuilder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+
+                    // Adapter 셋팅
+                    alertBuilder.setAdapter(adapter, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            // AlertDialog 안에 있는 AlertDialog
+                            switch (id) {
+                                //영문
+                                case 0:
+                                    Intent i0 = new Intent(Intent.ACTION_CALL, Uri.parse("tel:02-970-5081"));
+                                    try {
+                                        context.startActivity(i0);
+                                    } catch (Exception e) {}
+                                    break;
+                                case 1:
+                                    Intent i1 = new Intent(Intent.ACTION_CALL, Uri.parse("tel:02-970-5082"));
+                                    try {
+                                        context.startActivity(i1);
+                                    } catch (Exception e) {}
+                                    break;
+                                case 2:
+                                    Intent i2 = new Intent(Intent.ACTION_CALL, Uri.parse("tel:02-970-5082"));
+                                    try {
+                                        context.startActivity(i2);
+                                    } catch (Exception e) {}
+                                    break;
+                                case 3:
+                                    Intent i3 = new Intent(Intent.ACTION_CALL, Uri.parse("tel:02-970-5082"));
+                                    try {
+                                        context.startActivity(i3);
+                                    } catch (Exception e) {}
+                                    break;
+
+                                case 4:
+                                    Intent i4 = new Intent(Intent.ACTION_CALL, Uri.parse("tel:02-970-5082"));
+                                    try {
+                                        context.startActivity(i4);
+                                    } catch (Exception e) {}
+                                    break;
+
+                            } // end swtich
+                        }
+                    });
+
+            } //end if
+
+                else if (bean.getTeam().toString().equals("경비실")) {
+                    AlertDialog.Builder alertBuilder = new AlertDialog.Builder(context);
+                    alertBuilder.setTitle("번호를 선택하세요.");
+
+                    final ArrayAdapter<String> adapter = new ArrayAdapter<String>(context, android.R.layout.select_dialog_singlechoice);
+                    adapter.add("종합상황실");
+                    adapter.add("행정관-주관");
+                    adapter.add("정문");
+                    adapter.add("남문");
+                    adapter.add("도서관");
+                    adapter.add("바롬관1층");
+                    adapter.add("샬롬하우스");
+                    adapter.add("국제교육관");
+                    adapter.add("50주년기념관");
+                    adapter.add("아름관,다움관");
+
+                    alertBuilder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+
+                    // Adapter 셋팅
+                    alertBuilder.setAdapter(adapter, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            // AlertDialog 안에 있는 AlertDialog
+                            switch (id) {
+                                //종합상황실
+                                case 0:
+                                    Intent i0 = new Intent(Intent.ACTION_CALL, Uri.parse("tel:02-970-1000"));
+                                    try {
+                                        context.startActivity(i0);
+                                    } catch (Exception e) {
+                                        e.printStackTrace();
+                                    }
+                                    break;
+                                //행정관-주관
+                                case 1:
+                                    Intent i1 = new Intent(Intent.ACTION_CALL, Uri.parse("tel:02-970-5181"));
+                                    try {
+                                        context.startActivity(i1);
+                                    } catch (Exception e) {
+                                        e.printStackTrace();
+                                    }
+                                    break;
+                                //정문
+                                case 3:
+                                    Intent i3 = new Intent(Intent.ACTION_CALL, Uri.parse("tel:02-970-5183"));
+                                    try {
+                                        context.startActivity(i3);
+                                    } catch (Exception e) {
+                                        e.printStackTrace();
+                                    }
+                                    break;
+                                //남문
+                                case 4:
+                                    Intent i4 = new Intent(Intent.ACTION_CALL, Uri.parse("tel:02-970-5185"));
+                                    try {
+                                        context.startActivity(i4);
+                                    } catch (Exception e) {
+                                        e.printStackTrace();
+                                    }
+                                    break;
+                                //도서관
+                                case 5:
+                                    Intent i5 = new Intent(Intent.ACTION_CALL, Uri.parse("tel:02-970-5191"));
+                                    try {
+                                        context.startActivity(i5);
+                                    } catch (Exception e) {
+                                        e.printStackTrace();
+                                    }
+                                    break;
+                                //바롬관1층
+                                case 6:
+                                    Intent i6 = new Intent(Intent.ACTION_CALL, Uri.parse("tel:02-970-5193"));
+                                    try {
+                                        context.startActivity(i6);
+                                    } catch (Exception e) {
+                                        e.printStackTrace();
+                                    }
+                                    break;
+                                //샬롬하우스
+                                case 7:
+                                    Intent i7 = new Intent(Intent.ACTION_CALL, Uri.parse("tel:02-970-5194"));
+                                    try {
+                                        context.startActivity(i7);
+                                    } catch (Exception e) {
+                                        e.printStackTrace();
+                                    }
+                                    break;
+                                //국제교육관
+                                case 8:
+                                    Intent i8 = new Intent(Intent.ACTION_CALL, Uri.parse("tel:02-970-5198"));
+                                    try {
+                                        context.startActivity(i8);
+                                    } catch (Exception e) {
+                                        e.printStackTrace();
+                                    }
+                                    break;
+                                //50주년기념관
+                                case 9:
+                                    Intent i9 = new Intent(Intent.ACTION_CALL, Uri.parse("tel:02-970-7560"));
+                                    try {
+                                        context.startActivity(i9);
+                                    } catch (Exception e) {
+                                        e.printStackTrace();
+                                    }
+                                    break;
+                                //아름관,다움관
+                                case 10:
+                                    Intent i10 = new Intent(Intent.ACTION_CALL, Uri.parse("tel:02-970-7950"));
+                                    try {
+                                        context.startActivity(i10);
+                                    } catch (Exception e) {
+                                        e.printStackTrace();
+                                    }
+                                    break;
+                            } // end swtich
+                        }
+                    });
+
+                } // end else if*/
+
+                    {
+                    Toast.makeText(context, "전화 거는 중 : " + bean.getTeam(), Toast.LENGTH_SHORT).show();
+                    Intent i1 = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + bean.getTeam_Tel()));
+                    try {
+                        context.startActivity(i1);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                } // end else
+
+
 
             }
         });
