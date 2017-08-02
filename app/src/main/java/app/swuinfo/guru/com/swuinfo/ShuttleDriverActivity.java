b0d109997dc37c05c99c7aa1816a5861fdb8aab3
5562a@naver.com <MNBV9876@>
2017-08-02 13:57:42 +0900

    shuttle....

--- app/src/main/java/app/swuinfo/guru/com/swuinfo/ShuttleDriverActivity.java
+++ app/src/main/java/app/swuinfo/guru/com/swuinfo/ShuttleDriverActivity.java
@@ -9,5 +9,7 @@ public class ShuttleDriverActivity extends CommonActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver);
+
     }
+
 }
